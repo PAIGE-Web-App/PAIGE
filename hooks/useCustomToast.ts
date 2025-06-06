1d063219c50480748dd2bc7eafe5c2fca7f56a69
DYoon <dave.yoon92@gmail.com>
2025-06-05 23:11:21 -0500

    Rename useCustomtoast.ts to useCustomToast.ts to fix case sensitivity issue

--- hooks/useCustomToast.ts
+++ hooks/useCustomToast.ts
@@ -1,21 +1,20 @@
-// hooks/useCustomToast.ts (or utils/toastUtils.ts)
 import toast from "react-hot-toast";
 
 export const useCustomToast = () => {
   const showSuccessToast = (message: string) => {
-    toast.dismiss(); // Dismiss any existing toasts
+    toast.dismiss();
     toast.success(message);
   };
 
   const showErrorToast = (message: string) => {
-    toast.dismiss(); // Dismiss any existing toasts
+    toast.dismiss();
     toast.error(message);
   };
 
   const showInfoToast = (message: string) => {
-    toast.dismiss(); // Dismiss any existing toasts
-    toast(message); // Default toast for info/general messages
+    toast.dismiss();
+    toast(message);
   };
 
   return { showSuccessToast, showErrorToast, showInfoToast };
-};
+}; 
